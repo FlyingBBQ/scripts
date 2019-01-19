099ce823836522be60e9532f75b2225ff6a2bf53
FlyingBBQ <derek.cardol@gmail.com>
2019-01-19 18:55:07 +0100

    compact

--- mkey.sh
+++ mkey.sh
@@ -13,36 +13,21 @@ case "$1" in
         rm $setplayer
         exit
         ;;
-    *)
-        ;;
+    *) ;;
 esac
 
 if [ ! -e $setplayer ]; then
     case "$1" in
-        "play")
-            playerctl play-pause
-            ;;
-        "next")
-            playerctl next
-            ;;
-        "prev")
-            playerctl previous
-            ;;
-        *)
-            ;;
+        "play") playerctl play-pause ;;
+        "next") playerctl next ;;
+        "prev") playerctl previous ;;
+        *) ;;
     esac
 else
     case "$1" in
-        "play")
-            mpc -h ~/.config/mpd/socket toggle
-            ;;
-        "next")
-            mpc -h ~/.config/mpd/socket next
-            ;;
-        "prev")
-            mpc -h ~/.config/mpd/socket prev
-            ;;
-        *)
-            ;;
+        "play") mpc -h ~/.config/mpd/socket toggle ;;
+        "next") mpc -h ~/.config/mpd/socket next ;;
+        "prev") mpc -h ~/.config/mpd/socket prev ;;
+        *) ;;
     esac
 fi
