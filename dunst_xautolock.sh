d7be6fe4d78d6b03fbb0b999586d87ed29ba20b8
FlyingBBQ <derek.cardol@gmail.com>
2021-05-27 21:54:22 +0200

    Update locking

--- dunst_xautolock.sh
+++ dunst_xautolock.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # FlyingBBQ - xautolock notification
 
-TOGGLE=/tmp/.toggle
+toggle_file=/tmp/.toggle_autolock
+path_to_lock=/usr/bin/i3lock
 
-if [ ! -e $TOGGLE ]; then
-    touch $TOGGLE
+if [ ! -e $toggle_file ]; then
+    touch $toggle_file
     notify-send "xautolock disabled"
     pkill xautolock
 else
-    rm $TOGGLE
-    xautolock -time 15 -locker '/usr/bin/i3lock-fancy' &
+    rm $toggle_file
+    xautolock -time 15 -locker $path_to_lock &
     notify-send "xautolock enabled"
 fi
