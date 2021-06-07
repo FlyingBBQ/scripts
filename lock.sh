43de227f28630d359081456d855f087b5958f6ca
FlyingBBQ <derek.cardol@gmail.com>
2021-06-07 21:40:32 +0200

    Update arguments for i3lock

--- lock.sh
+++ lock.sh
@@ -2,31 +2,31 @@
 #
 # FlyingBBQ - i3lock configuration
 
-i3lock                      \
-\
---timecolor=ebdbb2ff        \
---datecolor=ebdbb2ff        \
---verifcolor=ebdbb2ff       \
---wrongcolor=ebdbb2ff       \
-\
---insidecolor=282828a0      \
---ringcolor=d79921ff        \
---keyhlcolor=458588ff       \
---bshlcolor=cc241dff        \
---insidevercolor=4585885a   \
---insidewrongcolor=cc241d5a \
---ringvercolor=83a598ff     \
---ringwrongcolor=fb4934ff   \
---line-uses-inside          \
-\
---screen 1                  \
---blur 6                    \
---clock                     \
---indicator                 \
-\
---datestr="%A, %b %d"       \
---timestr="%H:%M"           \
-\
---wrongtext="Nope!"         \
---show-failed-attempts      \
+i3lock                       \
+                             \
+--time-color=ebdbb2ff        \
+--date-color=ebdbb2ff        \
+--verif-color=ebdbb2ff       \
+--wrong-color=ebdbb2ff       \
+                             \
+--inside-color=282828a0      \
+--ring-color=d79921ff        \
+--keyhl-color=458588ff       \
+--bshl-color=cc241dff        \
+--insidever-color=4585885a   \
+--insidewrong-color=cc241d5a \
+--ringver-color=83a598ff     \
+--ringwrong-color=fb4934ff   \
+--line-uses-inside           \
+                             \
+--screen 1                   \
+--blur 6                     \
+--clock                      \
+--indicator                  \
+                             \
+--date-str="%A, %b %d"       \
+--time-str="%H:%M"           \
+                             \
+--wrong-text="Nope!"         \
+--show-failed-attempts       \
 --pass-media-keys
